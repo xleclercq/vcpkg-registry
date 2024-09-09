a7e280a47c6491b8c9740edd3d068585efda3110
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-09-09 16:09:47 +0100

    Updates

---
+++ ports/cmake-hello-world/portfile.cmake
@@ -0,0 +1,11 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO codesmithyide/cmake-tutorials
+    REF v1.0.0
+    SHA512 0
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_install()
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/cmake-tutorials" RENAME copyright)
